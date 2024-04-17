3d3b14e1ba3ec3ccddf2dfa554c93bff806946de
MauritusInvader <117859831+MauritusInvader@users.noreply.github.com>
2024-04-17 14:23:19 -0400

    🔧 (run-srcpy.bat): Fix issue in run-srcpy.bat script where missing backslash after OBS executable path caused the scrcpy executable to not run correctly. Added the missing backslash for proper execution.

--- run-srcpy.bat
+++ run-srcpy.bat
@@ -1,1 +1,2 @@
+"C:\Program Files\obs-studio\bin\64bit\obs64.exe" --scene "oculusstream" --startvirtualcam --minimize-to-tray	
 scrcpy-win64-v2.4\scrcpy.exe --max-size 640 --crop 2064:2064:0:0 --no-audio
